9f246477f1b51bd3bfb658283abb712ccb419ed0
Jerry Simmons GISP <fcemc@users.noreply.github.com>
2016-07-15 10:30:01 -0400

    Add files via upload

--- js/Initload.js
+++ js/Initload.js
@@ -335,18 +335,28 @@ function getMemberScanInfo(paramItems) {
         success: function (result) {
             var results = result.MEMBERLISTSCANResult;
             if (results.length == 1) {
-                var string = "";
-                string += "<div><b>NAME: </b><label style='display:inline-block' id='logmem_NAME>" + results[0].NAME.toString() + "</label></div>";
-                string += "<div><b>PHONE: </b><label style='display:inline-block' id='logmem_PHONE>" + results[0].PHONE.toString() + "</label></div>";
-                string += "<div><b>MEMBERNO: </b><label style='display:inline-block' id='logmem_MEMBERNO>" + results[0].MEMBERNO.toString() + "</label></div>";
-                string += "<div><b>MEMBERSEP: </b><label style='display:inline-block' id='logmem_MEMBERSEP>" + results[0].MEMBERSEP.toString() + "</label></div>";
-                string += "<div><b>BILLADDR: </b><label style='display:inline-block' id='logmem_BILLADDR>" + results[0].BILLADDR.toString() + "</label></div>";
-                string += "<div><b>SERVADDR: </b><label style='display:inline-block' id='logmem_SERVADDR>" + results[0].SERVADDR.toString() + "</label></div>";
-                string += "<div><b>MAPNUMBER: </b><label style='display:inline-block' id='logmem_MAPNUMBER>" + results[0].MAPNUMBER.toString() + "</label></div>";
-                string += "<div><b>METER: </b><label style='display:inline-block' id='logmem_METER>" + results[0].METER.toString() + "</label></div>";
+                //var string = "";
+                //string += "<div><b>NAME: </b><label style='display:inline-block' id='logmem_NAME>" + results[0].NAME.toString() + "</label></div>";
+                //string += "<div><b>PHONE: </b><label style='display:inline-block' id='logmem_PHONE>" + results[0].PHONE.toString() + "</label></div>";
+                //string += "<div><b>MEMBERNO: </b><label style='display:inline-block' id='logmem_MEMBERNO>" + results[0].MEMBERNO.toString() + "</label></div>";
+                //string += "<div><b>MEMBERSEP: </b><label style='display:inline-block' id='logmem_MEMBERSEP>" + results[0].MEMBERSEP.toString() + "</label></div>";
+                //string += "<div><b>BILLADDR: </b><label style='display:inline-block' id='logmem_BILLADDR>" + results[0].BILLADDR.toString() + "</label></div>";
+                //string += "<div><b>SERVADDR: </b><label style='display:inline-block' id='logmem_SERVADDR>" + results[0].SERVADDR.toString() + "</label></div>";
+                //string += "<div><b>MAPNUMBER: </b><label style='display:inline-block' id='logmem_MAPNUMBER>" + results[0].MAPNUMBER.toString() + "</label></div>";
+                //string += "<div><b>METER: </b><label style='display:inline-block' id='logmem_METER>" + results[0].METER.toString() + "</label></div>";
 
-                $("#memberData").empty();
-                $("#memberData").append(string);
+
+                var memData = [];
+                memData.push("NAME|" + results[0].NAME.toString());
+                memData.push("PHONE|" + results[0].PHONE.toString());
+                memData.push("MEMBERNO|" + results[0].NAME.toString());
+                memData.push("MEMBERSEP|" + results[0].MEMBERSEP.toString());
+                memData.push("BILLADDR|" + results[0].BILLADDR.toString());
+                memData.push("SERVADDR|" + results[0].SERVADDR.toString());
+                memData.push("MAPNUMBER|" + results[0].MAPNUMBER.toString());
+                memData.push("METER|" + results[0].METER.toString());
+                beginCheckIn(memData);
+                
                 changePage('checkInPage');
             }
             else if (results.length > 1) {
